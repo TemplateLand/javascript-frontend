bb8bdef6f6c74132f2dbad8ba717d9407da5a529
chengluyu <chengluyu@live.cn>
2020-01-17 10:47:40 +0800

    Fix: `contenthash` should be `hash`

--- webpack.config.js
+++ webpack.config.js
@@ -8,7 +8,7 @@ module.exports = {
   entry: path.join(__dirname, 'src', 'index.js'),
   output: {
     path: path.join(__dirname, 'dist'),
-    filename: '[name].[contenthash].js',
+    filename: '[name].[hash].js',
   },
   devtool: 'inline-source-map',
   devServer: {
